5f52d212ef271accc36a877e6b2454e35304f6ce
Jongyeol Yang <yangjy0113@gmail.com>
2017-02-18 20:19:44 +0900

    버전처리 함수 변경

--- kiwoom/script/process_version.py
+++ kiwoom/script/process_version.py
@@ -6,16 +6,24 @@ import os
 app = application.Application()
 app.start("C:/Program Files/Kiwoom/KiwoomFlash2/khministarter.exe")
 
+account = []
+with open("../../data/account.txt", 'r') as f:
+    account = f.readlines()
+
 title = "번개 Login"
 dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
 
+idForm = dlg.Edit0
+idForm.SetFocus()
+idForm.TypeKeys(account[0])
+
 pass_ctrl = dlg.Edit2
 pass_ctrl.SetFocus()
-pass_ctrl.TypeKeys('nacl153')
+pass_ctrl.TypeKeys(account[1])
 
 cert_ctrl = dlg.Edit3
 cert_ctrl.SetFocus()
-cert_ctrl.TypeKeys('Didwhdduf10!')
+cert_ctrl.TypeKeys(account[2])
 
 btn_ctrl = dlg.Button0
 btn_ctrl.Click()
